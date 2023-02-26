a4be94784150fe746047af4099040709c516b466
[Freya <[zhifangzhang.zzf@gmail.com]>
2023-02-26 16:51:07 -0600

    finalVersion

--- src/components/DragNDrop.js
+++ src/components/DragNDrop.js
@@ -1,12 +1,15 @@
 import {data} from "../consts";
 import {useState, useRef} from "react";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export const DragNDrop = () => {
 
     //const tasks and dragging tasks
     const [tasks, setTasks] = useState(data)
+    console.log(tasks)
     const dragItem = useRef()
     const dragNode = useRef()
+
     //dragging item
 
     const [dragging, setDragging] = useState(false)
@@ -22,11 +25,11 @@ export const DragNDrop = () => {
     }
     const draggingEnter = (e, params) => {
         const currentItem = dragItem.current
-        if(e.target !== dragNode.current){
+        if (e.target !== dragNode.current) {
             setTasks(oldList => {
                 let newList = JSON.parse(JSON.stringify(oldList))
-                newList[params.groupIndex].items.splice(params.taskIndex,0,newList[currentItem.groupIndex].items
-                    .splice(currentItem.taskIndex,1)[0])
+                newList[params.groupIndex].items.splice(params.taskIndex, 0, newList[currentItem.groupIndex].items
+                    .splice(currentItem.taskIndex, 1)[0])
                 dragItem.current = params
                 return newList
             })
@@ -52,31 +55,84 @@ export const DragNDrop = () => {
         return `dndItem group${params.groupIndex}`
 
     }
+    // add task
+    const [task, setTask] = useState('')
+    console.log(task)
+    const addTask = (newTask) => {
+
+        newTask ? setTasks(oldList => {
+                let newList = JSON.parse(JSON.stringify(oldList))
+                newList[0].items.push(task)
+                return newList
+
+            }
+        ) : alert('please input a valid task name')
+    }
+    const inputNewTask = (e, newTask) => {
+
+        e.code === 'Enter' && setTasks(oldList => {
+                let newList = JSON.parse(JSON.stringify(oldList))
+                newList[0].items.push(task)
+                return newList
+
+            }
+        )
+        e.code === 'Enter' && !newTask && alert('please input a valid task name')
+    }
+    //delete item
+    const deleteItem = (params) => {
+        setTasks(oldList => {
+            let newList = JSON.parse(JSON.stringify(oldList))
+            newList[params.groupIndex].items.splice(params.taskIndex, 1)
+
+
+            return newList
+        })
+    }
+
+
     return (
         <div className="app">
+            <div className="addTask">
+
+                <input type="text" name="task" placeholder="add a new task here"
+                       onChange={e => setTask(e.target.value)} onKeyDown={(e) => inputNewTask(e, task)}/>
+                <button onClick={() => addTask(task)}>Add task</button>
+            </div>
             <div className="appHeader">
+                {/*add tack*/}
 
+                {/*kanban board*/}
                 {tasks.map((item, groupIndex) =>
-                    <div className= 'dndGroup'
+                    <div className='dndGroup'
 
-                    onDragEnter={
-                        dragging && !item.items.length? (e) => draggingEnter(e, {groupIndex, taskIndex: 0}):null}
+                         onDragEnter={
+                             dragging && !item.items.length ? (e) => draggingEnter(e, {
+                                 groupIndex,
+                                 taskIndex: 0
+                             }) : null}
                     >
-                        <div className= 'title'> {item.title}</div>
+                        <div className='title'> {item.title}</div>
                         <div className='dndItems'>
                             {
                                 item.items.map((task, taskIndex) =>
                                     <div
                                         draggable
-                                         onDragStart={(e) => {
-                                             dragStartHandler(e, {groupIndex, taskIndex})
-                                         }}
-                                        onDragEnter={dragging? (e) => {draggingEnter(e, {groupIndex, taskIndex})}: null}
-                                         className={dragging ? draggingStyle({groupIndex, taskIndex}) : `dndItem group${groupIndex}`}
-                                        >
+                                        onDragStart={(e) => {
+                                            dragStartHandler(e, {groupIndex, taskIndex})
+                                        }}
+                                        onDragEnter={dragging ? (e) => {
+                                            draggingEnter(e, {groupIndex, taskIndex})
+                                        } : null}
+                                        className={dragging ? draggingStyle({
+                                            groupIndex,
+                                            taskIndex
+                                        }) : `dndItem group${groupIndex}`}
+                                    >
 
 
-                                        {task}
+                                        <div>{task}</div>
+                                        <div><DeleteForeverIcon color= 'grey' onClick={() => deleteItem({groupIndex, taskIndex})}/></div>
                                     </div>)
 
 
--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const data = [
-    {title: 'To Do', items: ['Task 1', 'Task 2', 'Task 3']},
+    {title: 'To Do', items: ['Task 1', 'Task 2', 'Task 3', "Task 4", "Task 5"]},
     {title: 'In Progress', items: []},
     {title: 'Done', items: []},
     {title: 'Pending', items: []},
